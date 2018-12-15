5a3fdec8fc5a6bbdb0a1474945ed0c5df1be5898
Julien DOCHE <julien.doche@live.fr>
2018-12-15 14:22:57 +0100

    ModGeoInv: Fix radian to degree
    Signed-off-by: Julien DOCHE <julien.doche@live.fr>

--- ModGeoInv.m
+++ ModGeoInv.m
@@ -1,4 +1,4 @@
 function [out] = ModGeoInv(X, Y, a1, a2)
-	t2 = acos((X^2 + Y^2 - a1^2 - a2^2) / (2 * a1 * a2));
-	t1 = atan2(Y, X) - atan2(a2 * sin(t2), a1 + a2 * cos(t2));
+	t2 = acosd((X^2 + Y^2 - a1^2 - a2^2) / (2 * a1 * a2));
+	t1 = atan2d(Y, X) - atan2d(a2 * sind(t2), a1 + a2 * cosd(t2));
 out = [t1 t2];
--- Trajectory_PLaning_prg.m
+++ Trajectory_PLaning_prg.m
@@ -58,11 +58,9 @@ if  Xf > a1
 	flag = 1;
 end
 
-[q1i] = Scara_GEOINV(Xi, Yi);
-[q2i] = Scara_GEOINV(Xi, Yi);
 
-[q1f] = Scara_GEOINV(Xf, Yf);
-[q2f] = Scara_GEOINV(Xf, Yf);
+[thetai] = ModGeoInv(Xi, Yi, a1, a2);
+[thetaf] = ModGeoInv(Xf, Yf, a1, a2);
 
 % spline d'ordre 5 pour trajectoire initiale et finale
 % à modifier lorqu'on travaille pour le scara pour chaque articulation
@@ -74,8 +72,13 @@ A = [1, to, ((to)^2), ((to)^3), ((to)^4), ((to)^5);     % pos init
 0, 1, 2 * tf, 3 * ((tf)^2), 4 * ((tf)^3), 5 * ((tf)^4); % vit final
 0, 0, 2, 6 * tf, 12 * ((tf)^2), 20 * ((tf)^3)];         % acc final
 
-B1 = [q1i; Vo; ao; q1f; Vf; af];
-B2 = [q2i; Vo; ao; q2f; Vf; af];
+disp(thetai(1));
+disp(thetaf(1));
+disp(thetai(2));
+disp(thetaf(2));
+
+B1 = [thetai(1); Vo; ao; thetaf(1); Vf; af];
+B2 = [thetai(2); Vo; ao; thetaf(2); Vf; af];
 
 C1 = inv(A) * B1;
 C2 = inv(A) * B2;
@@ -89,12 +92,12 @@ V1 = [];     % tableau des vitesses arti 1
 AC1 = [];    % tableau des vitesses arti 2
 
 for t = to:.01:tf-.01
-	q1 = C1(1, 1) + C1(2, 1) * t + C1(3, 1) * t^2 + C1(4, 1) * t^3 + C1(5, 1) * t^4 + C1(6, 1) * t^5;  
-	v1 = C1(2, 1) + (2 * C1(3, 1) * t) + (3 * C1(4, 1) * t^2) + (4 * C1(5, 1) * t^3) + (5 * C1(6, 1) * t^4); 
+	q1 = C1(1, 1) + C1(2, 1) * t + C1(3, 1) * t^2 + C1(4, 1) * t^3 + C1(5, 1) * t^4 + C1(6, 1) * t^5;
+	v1 = C1(2, 1) + (2 * C1(3, 1) * t) + (3 * C1(4, 1) * t^2) + (4 * C1(5, 1) * t^3) + (5 * C1(6, 1) * t^4);
 	ac1 = (2 * C1(3, 1)) + (6 * C1(4, 1) * t) + (12 * C1(5, 1) * t^2) + (20 * C1(6, 1) * t^3);
 
-	q2 = C2(1, 1) + C2(2, 1) * t + C2(3, 1) * t^2 + C2(4, 1) * t^3 + C2(5, 1) * t^4 + C2(6, 1) * t^5;  
-	v2 = C2(2, 1) + (2 * C2(3, 1) * t) + (3 * C2(4, 1) * t^2) + (4 * C2(5, 1) * t^3) + (5 * C2(6, 1) * t^4); 
+	q2 = C2(1, 1) + C2(2, 1) * t + C2(3, 1) * t^2 + C2(4, 1) * t^3 + C2(5, 1) * t^4 + C2(6, 1) * t^5;
+	v2 = C2(2, 1) + (2 * C2(3, 1) * t) + (3 * C2(4, 1) * t^2) + (4 * C2(5, 1) * t^3) + (5 * C2(6, 1) * t^4);
 	ac2 = (2 * C2(3, 1)) + (6 * C2(4, 1) * t) + (12 * C2(5, 1) * t^2) + (20 * C2(6, 1) * t^3);
 
 	x = a1 * cosd(q1);
@@ -137,7 +140,7 @@ for i = 1:1:length(Q1)
 	hold on
 	plot3([Xf Xf], [Yf  Yf], [Zi Zf], 'green', 'linewidth', 3)
 	hold on
-	plot3(X, Y, Z, 'green', 'linewidth', 3)
+	plot3(X, Y, Z, 'blue', 'linewidth', 3)
 	hold on
 	Dessiner_SCARA(Q1(i), Q2(i), a1, d1, a2)
 
